f747abfdf10c39c28b4a3736431e4371d2849090
Anna Thomas <anna.thomas@infosum.com>
2020-11-06 17:05:12 +0000

    Compile js files

---
+++ js/it.js
@@ -0,0 +1,48 @@
+/// <reference types='cypress' />
+var isMochaFunc = function (fn) { return typeof fn === 'function'; };
+var isMochaTitle = function (title) { return typeof title === 'string'; };
+var isMochaTags = function (title) { return typeof title === 'object'; };
+var mochaIt = it;
+var itWithTags = function (p1, p2, p3) {
+    var includeTags = Cypress.env('TAGS') ? Cypress.env('TAGS').split(',') : [];
+    var excludeTags = Cypress.env('NOT_TAGS') ? Cypress.env('NOT_TAGS').split(',') : [];
+    if (isMochaFunc(p1)) {
+        return mochaIt(p1);
+    }
+    if (isMochaTitle(p1) && isMochaFunc(p2)) {
+        if (includeTags.length) {
+            // If include tags have been provided, skip any untagged tests
+            return;
+        }
+        return mochaIt(p1, p2);
+    }
+    if (isMochaTags(p1) && isMochaTitle(p2) && isMochaFunc(p3)) {
+        var tags = p1;
+        var title = p2;
+        var fn = p3;
+        if (includeTags.length || excludeTags.length) {
+            var includeTest = false;
+            var excludeTest = false;
+            includeTest = includeTags.length === 0 || tags.some(function (tag) { return includeTags.includes(tag); });
+            excludeTest = excludeTags.length > 0 && tags.some(function (tag) { return excludeTags.includes(tag); });
+            var runTest = includeTest && !excludeTest;
+            if (runTest) {
+                // Include tag found, run test
+                return mochaIt(title, fn);
+            }
+            else {
+                // Include tag not found or excluded tag found, skip test
+                return;
+            }
+        }
+        else {
+            // If not tags have been provided, run all tests
+            return mochaIt(title, fn);
+        }
+    }
+};
+// Pass through OG mocha it methods
+itWithTags.skip = mochaIt.skip;
+itWithTags.only = mochaIt.only;
+itWithTags.retries = mochaIt.retries;
+it = itWithTags;
