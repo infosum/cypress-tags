9cc3eb0b65cd87c5b6b28d948532712385787863
Anna Thomas <anna.thomas@infosum.com>
2020-11-09 12:11:09 +0000

    Reference cypress types through module instead of relative path

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,31 +7,7 @@
 // TypeScript Version: 3.4
 // Updated by the Cypress team: https://www.cypress.io/about/
 
-/// <reference path="../node_modules/cypress/types/cy-blob-util.d.ts" />
-/// <reference path="../node_modules/cypress/types/cy-bluebird.d.ts" />
-/// <reference path="../node_modules/cypress/types/cy-moment.d.ts" />
-/// <reference path="../node_modules/cypress/types/cy-minimatch.d.ts" />
-/// <reference path="../node_modules/cypress/types/cy-chai.d.ts" />
-/// <reference path="../node_modules/cypress/types/lodash/index.d.ts" />
-/// <reference path="../node_modules/cypress/types/sinon/index.d.ts" />
-/// <reference path="../node_modules/cypress/types/sinon-chai/index.d.ts" />
-/// <reference path="../node_modules/cypress/types/mocha/index.d.ts" />
-/// <reference path="../node_modules/cypress/types/jquery/index.d.ts" />
-/// <reference path="../node_modules/cypress/types/chai-jquery/index.d.ts" />
-
-// jQuery includes dependency "sizzle" that provides types
-// so we include it too in "node_modules/sizzle".
-// This way jQuery can load it using 'reference types="sizzle"' directive
-
-// load ambient declaration for "cypress" NPM module
-// hmm, how to load it better?
-/// <reference path="../node_modules/cypress/types/cypress-npm-api.d.ts" />
-
-/// <reference path="../node_modules/cypress/types/net-stubbing.d.ts" />
-/// <reference path="../node_modules/cypress/types/cypress.d.ts" />
-/// <reference path="../node_modules/cypress/types/cypress-global-vars.d.ts" />
-/// <reference path="../node_modules/cypress/types/cypress-type-helpers.d.ts" />
-/// <reference path="../node_modules/cypress/types/cypress-expect.d.ts" />
+/// <reference types="cypress" />
 
 // Add tags to mocha definitions
 /// <reference path="./cypress-tags.d.ts" />
