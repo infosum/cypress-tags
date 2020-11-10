f42ccdfe4dd934a4f71f659bd671a54227b2dd85
Anna Thomas <anna.thomas@infosum.com>
2020-11-10 17:41:08 +0000

    Extend test cases

--- src/tagify.ts
+++ src/tagify.ts
@@ -3,9 +3,8 @@ import ts, { factory } from 'typescript';
 
 const browserify = require('@cypress/browserify-preprocessor');
 
-const isTestBlock = (name: string) => (node: ts.Node) => {
-  return ts.isCallExpression(node) &&
-    ts.isIdentifier(node.expression) &&
+const isTestBlock = (name: string) => (node: ts.CallExpression | ts.PropertyAccessExpression) => {
+  return ts.isIdentifier(node.expression) &&
     node.expression.escapedText === name;
 };
 
@@ -13,59 +12,110 @@ const isDescribe = isTestBlock('describe');
 const isContext = isTestBlock('context');
 const isIt = isTestBlock('it');
 
-const transformer = <T extends ts.Node>(context: ts.TransformationContext) => (rootNode: T) => {
-  const visit = (node: ts.Node, parentTags?: string[]): ts.Node => {
+const extractTags = () => {
+  const includeTags = process.env.CYPRESS_INCLUDE_TAGS ? process.env.CYPRESS_INCLUDE_TAGS.split(',') : [];
+  const excludeTags = process.env.CYPRESS_EXCLUDE_TAGS ? process.env.CYPRESS_EXCLUDE_TAGS.split(',') : [];
 
+  return {
+    includeTags,
+    excludeTags,
+  };
+};
+
+const removeTagsFromNode = (node: ts.Node, parentTags: string[], includeTags: string[], excludeTags: string[]): {
+  node: ts.Node,
+  tags: string[],
+  skipChildren: boolean,
+} => {
+  if (ts.isCallExpression(node)) {
+    const firstArg = node.arguments[0];
+    if (ts.isArrayLiteralExpression(firstArg)) {
+      const nodeTags = firstArg.elements.map((element: any) => element.text);
+      const uniqueTags = [...new Set([...nodeTags, ...parentTags])];
+
+      const includeTest = includeTags.length === 0 || uniqueTags.some(tag => includeTags.includes(tag));
+      const excludeTest = excludeTags.length > 0 && uniqueTags.some(tag => excludeTags.includes(tag));
+
+      const skipChildren = !(includeTest && !excludeTest);
+
+      // Create a new node removing the tag list as the first argument
+      const newArgs: ts.NodeArray<ts.Expression> = factory.createNodeArray([...node.arguments.slice(1)]);
+      const newExpression = factory.createCallExpression(node.expression, undefined, newArgs);
+      return {
+        node: newExpression,
+        tags: uniqueTags,
+        skipChildren,
+      };
+    }
+  }
+  return {
+    node,
+    tags: parentTags,
+    skipChildren: false,
+  };
+}
+
+const transformer = <T extends ts.Node>(context: ts.TransformationContext) => (rootNode: T) => {
+  const { includeTags, excludeTags } = extractTags();
+
+  const visit = (node: ts.Node, parentTags?: string[]): ts.Node | undefined => {
     let tags: string[] = parentTags ?? [];
     let returnNode = node;
 
-    if (ts.isCallExpression(node) && ts.isIdentifier(node.expression)) {
+    let skipChildren = false;
+
+    if (ts.isCallExpression(node)) {
       const firstArg = node.arguments[0];
 
-      if (isDescribe(node) || isContext(node)) {
-        if (ts.isArrayLiteralExpression(firstArg)) {
-          // First arg is tags list
-          const newTags = firstArg.elements.map((element: any) => element.text);
-          tags = [...(parentTags ?? []), ...newTags];
-        }
-      } else if (isIt(node)) {
-        if (ts.isStringLiteral(firstArg)) {
-          // First arg is title
-          if (parentTags?.length) {
-            // Create a new node with parent tag list as the first argument
-            const elements = parentTags.map((tag) => factory.createStringLiteral(tag, true));
-            const tagsArg = factory.createArrayLiteralExpression(elements, false);
-
-            const newArgs: ts.NodeArray<ts.Expression> = factory.createNodeArray([tagsArg, ...node.arguments]);
-            const newExpression = factory.createCallExpression(node.expression, undefined, newArgs);
-            ts.setSourceMapRange(newExpression, ts.getSourceMapRange(node))
-            returnNode = newExpression;
+      if (ts.isIdentifier(node.expression)) {
+        if (isDescribe(node) || isContext(node)) {
+          if (ts.isArrayLiteralExpression(firstArg)) {
+            // First arg is tags list
+            const result = removeTagsFromNode(node, tags, includeTags, excludeTags);
+            skipChildren = result.skipChildren;
+            returnNode = result.node;
+            tags = result.tags;
           }
-        } else if (ts.isArrayLiteralExpression(firstArg)) {
-          // First arg is tags list
-          if (parentTags?.length) {
-            // Create a new node with combined tag lists as the first argument
-            const nodeTags = firstArg.elements.map((element: any) => element.text);
-            const uniqueTags = [...new Set([...nodeTags, ...parentTags])];
-            const elements = uniqueTags.map((tag) => factory.createStringLiteral(tag, true));
-            const tagsArg = factory.createArrayLiteralExpression(elements, false);
+        } else if (isIt(node)) {
+          if (ts.isStringLiteral(firstArg)) {
+            // First arg is title
+            const includeTest = includeTags.length === 0 || tags.some(tag => includeTags.includes(tag));
+            const excludeTest = excludeTags.length > 0 && tags.some(tag => excludeTags.includes(tag));
 
-            const newArgs: ts.NodeArray<ts.Expression> = factory.createNodeArray([tagsArg, ...node.arguments.slice(1)]);
-            const newExpression = factory.createCallExpression(node.expression, undefined, newArgs);
-            returnNode = newExpression;
+            skipChildren = !(includeTest && !excludeTest);
+          } else if (ts.isArrayLiteralExpression(firstArg)) {
+            // First arg is tags list
+            const result = removeTagsFromNode(node, tags, includeTags, excludeTags);
+            skipChildren = result.skipChildren;
+            returnNode = result.node;
+          }
+        }
+      } else if (ts.isPropertyAccessExpression(node.expression)) {
+        // Node contains a .skip or .only
+        if (isIt(node.expression)) {
+          if (ts.isArrayLiteralExpression(firstArg)) {
+            // First arg is tags list
+            const result = removeTagsFromNode(node, tags, includeTags, excludeTags);
+            skipChildren = result.skipChildren;
+            returnNode = result.node;
           }
         }
       }
     }
 
-    returnNode = ts.visitEachChild(returnNode, (node) => visit(node, tags), context);
+    if (skipChildren) {
+      returnNode = factory.createEmptyStatement();
+    } else {
+      returnNode = ts.visitEachChild(returnNode, (node) => visit(node, tags), context);
+    }
 
     return returnNode;
   };
+
   return ts.visitNode(rootNode, visit);
 };
 
-const process = (fileName: string, source: string) => {
+const processFile = (fileName: string, source: string) => {
   const printer = ts.createPrinter();
 
   const sourceFile = ts.createSourceFile(fileName, source, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
@@ -85,14 +135,14 @@ const transform = (fileName: string) => {
   }
 
   function onend (this: through.ThroughStream) {
-    this.queue(process(fileName, data));
+    this.queue(processFile(fileName, data));
     this.emit('end');
   }
 
   return through(ondata, onend);
 };
 
-const onFilePreprocessor = () => {
+const preprocessor = () => {
   const options = {
     ...browserify.defaultOptions,
     typescript: require.resolve('typescript'),
@@ -109,7 +159,6 @@ const onFilePreprocessor = () => {
   return browserify(options);
 };
 
-module.exports = {
-  onFilePreprocessor,
-  transform,
-};
+preprocessor.transform = transform;
+
+module.exports = preprocessor;
--- test/tagify.js
+++ test/tagify.js
@@ -3,61 +3,148 @@ const browserify = require('browserify');
 const expect = require('chai').expect;
 const transform = require('../dist/tagify').transform;
 
+const tagify = () => {
+  return new Promise((resolve) => {
+    const options = {
+      ...browserify.defaultOptions,
+      typescript: require.resolve('typescript'),
+    };
+
+    try {
+      browserify(options)
+        .transform(transform)
+        .add(__dirname + '/../example/sample.ts')
+        .bundle()
+        .pipe(through(ondata, onend));
+
+      let data = ''
+      function ondata(d) { data += d }
+      function onend() {
+        const lines = data.split('\n')
+        const startline = lines.indexOf('// sample start') + 1;
+        const endline = lines.indexOf('// sample end');
+
+        output = lines.slice(startline, endline);
+        resolve(output);
+      }
+    } catch (err) {
+      console.log('I have an error');
+      reject(output);
+    }
+  });
+};
+
 describe('Tagify', function() {
   let output = '';
 
-  before(() => {
-    return new Promise((resolve) => {
-      const options = {
-        ...browserify.defaultOptions,
-        typescript: require.resolve('typescript'),
-      };
+  beforeEach(() => {
+    delete process.env.CYPRESS_INCLUDE_TAGS;
+    delete process.env.CYPRESS_EXCLUDE_TAGS;
+  });
 
-      try {
-        // tagify().transform(__dirname + '/../example/sample.ts');
-        browserify(options)
-          .transform(transform)
-          .add(__dirname + '/../example/sample.ts')
-          .bundle()
-          .pipe(through(ondata, onend));
+  describe('No tags provided', () => {
+    before(async () => {
+      output = await tagify();
+    });
 
-        let data = ''
-        function ondata(d) { data += d }
-        function onend() {
-          const lines = data.split('\n')
-          const startline = lines.indexOf('// sample start') + 1;
-          const endline = lines.indexOf('// sample end');
-
-          output = lines.slice(startline, endline);
-          resolve(output);
-        }
-      } catch (err) {
-        expect(err).to.not.exist;
-      }
+    it('should output all tests without tags', function() {
+      expect(output).to.deep.equal([
+        "describe('Run tests with no tags', () => {",
+        "    it('I am a regular test', () => { });",
+        "});",
+        "describe('Run tests with tagged describe block', () => {",
+        "    it('I will become a wip test', () => { });",
+        "    it('I will also become a wip test', () => { });",
+        "    it('I am already a wip test', () => { });",
+        "    it.skip('I should always be skipped', () => { });",
+        "});",
+        "describe('Run tests with tagged it statements', () => {",
+        "    it('I am a wip test', () => { });",
+        "    it('I am a smoke & regression test', () => { });",
+        "    it('I am a regression test', () => { });",
+        "    it('I am a smoke test', () => { });",
+        "    it('I am a wip smoke test', () => { });",
+        "    it.skip('I have tags and should always be skipped', () => { });",
+        "});",
+      ]);
     });
   });
 
-  it('should not edit tests without tags', function() {
-    expect(output.slice(0, 3)).to.deep.equal([
-      `describe('No tag tests', () => {`,
-      `    it('I am a regular test', () => { });`,
-      `});`,
-    ]);
+  describe('Include tags provided', () => {
+    before(async () => {
+      process.env.CYPRESS_INCLUDE_TAGS='wip';
+      output = await tagify();
+    });
+
+    it('should output all tests without tags', function() {
+      expect(output).to.deep.equal([
+        "describe('Run tests with no tags', () => {",
+        "    ;",
+        "});",
+        "describe('Run tests with tagged describe block', () => {",
+        "    it('I will become a wip test', () => { });",
+        "    it('I will also become a wip test', () => { });",
+        "    it('I am already a wip test', () => { });",
+        "    it.skip('I should always be skipped', () => { });",
+        "});",
+        "describe('Run tests with tagged it statements', () => {",
+        "    it('I am a wip test', () => { });",
+        "    ;",
+        "    ;",
+        "    ;",
+        "    it('I am a wip smoke test', () => { });",
+        "    it.skip('I have tags and should always be skipped', () => { });",
+        "});",
+      ]);
+    });
   });
 
-  it('should not remove tags from describe block', function() {
-    expect(output[3]).to.equal(`describe(['wip'], 'WIP tests', () => {`);
+  describe('Exclude tags provided', () => {
+    before(async () => {
+      process.env.CYPRESS_EXCLUDE_TAGS='wip';
+      output = await tagify();
+    });
+
+    it('should output all tests without tags', function() {
+      expect(output).to.deep.equal([
+        "describe('Run tests with no tags', () => {",
+        "    it('I am a regular test', () => { });",
+        "});",
+        ";",
+        "describe('Run tests with tagged it statements', () => {",
+        "    ;",
+        "    it('I am a smoke & regression test', () => { });",
+        "    it('I am a regression test', () => { });",
+        "    it('I am a smoke test', () => { });",
+        "    ;",
+        "    ;",
+        "});",
+      ]);
+    });
   });
 
-  it('should add parent tags to test without tags', function() {
-    expect(output[4]).to.equal(`    it(['wip'], 'I will become a wip test', () => { });`);
-  });
+  describe('Include and exclude tags provided', () => {
+    before(async () => {
+      process.env.CYPRESS_INCLUDE_TAGS='smoke,regression';
+      process.env.CYPRESS_EXCLUDE_TAGS='wip';
+      output = await tagify();
+    });
 
-  it('should append parent tags to test with tags', function() {
-    expect(output[5]).to.equal(`    it(['feature', 'wip'], 'I will also become a wip test', () => { });`);
-  });
-
-  it('should not add parent tags to test with the same tags', function() {
-    expect(output[6]).to.equal(`    it(['wip'], 'I am already a wip test', () => { });`);
+    it('should output all tests without tags', function() {
+      expect(output).to.deep.equal([
+        "describe('Run tests with no tags', () => {",
+        "    ;",
+        "});",
+        ";",
+        "describe('Run tests with tagged it statements', () => {",
+        "    ;",
+        "    it('I am a smoke & regression test', () => { });",
+        "    it('I am a regression test', () => { });",
+        "    it('I am a smoke test', () => { });",
+        "    ;",
+        "    ;",
+        "});",
+      ]);
+    });
   });
 });
