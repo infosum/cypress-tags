8d8da7810b102c3ce6397afb88009e673c1d7025
Anna Thomas <anna.thomas@infosum.com>
2022-08-02 11:27:46 +0100

    Use single quotes and add missing reference path

--- cypress/e2e/fixtures.cy.ts
+++ cypress/e2e/fixtures.cy.ts
@@ -1,19 +1,19 @@
 /// <reference path='../../dist/index.d.ts' />
 
-import fixture from "../fixtures/example.json";
+import fixture from '../fixtures/example.json';
 
-describe("Uses fixtures", () => {
+describe('Uses fixtures', () => {
   beforeEach(() => {
-    cy.fixture("example").as("fixture");
+    cy.fixture('example').as('fixture');
   });
 
-  it("should load fixture", function () {
-    expect(this.fixture.foo).to.equal("bar");
-    expect(fixture.foo).to.equal("bar");
+  it('should load fixture', function () {
+    expect(this.fixture.foo).to.equal('bar');
+    expect(fixture.foo).to.equal('bar');
   });
 
-  it(["wip"], "should load fixture when filtered", function () {
-    expect(this.fixture.foo).to.equal("bar");
-    expect(fixture.foo).to.equal("bar");
+  it(['wip'], 'should load fixture when filtered', function () {
+    expect(this.fixture.foo).to.equal('bar');
+    expect(fixture.foo).to.equal('bar');
   });
 });
